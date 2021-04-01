e0caa9e23cedbbfefc907739116cc00dd3a9b416
brijeshbiet88@gmail.com <brijeshbiet88@gmail.com>
2021-04-01 12:32:21 +0530

    Patch Method Demo

--- src/main/java/com/example/controller/CustomerController.java
+++ src/main/java/com/example/controller/CustomerController.java
@@ -1,11 +1,14 @@
 package com.example.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -45,6 +48,23 @@ public class CustomerController {
 		List<Customer> customersByType = customerService.findCustomerByAccountType(type);
 		
 		return new ResponseEntity<List<Customer>>(customersByType , HttpStatus.OK);
+		
+	}
+	
+	@RequestMapping(value = "/patch" , method = RequestMethod.PATCH , produces = "application/json" , consumes = "application/json")
+	public ResponseEntity<?> pathDemo(@RequestBody Map <String, Object> updates){
+	  
+		Map<String , Object> response = new HashMap<String, Object>();
+		response.put("patch", "Patch Request Demo with Given fields in the map through postman");
+		response.put("School", "ABC");
+		response.put("Height", 4.7);
+		
+		updates.forEach((k , v) -> {
+			response.put(k, v);
+			System.out.println("Key : "+k);
+		});
+		
+		return new ResponseEntity<>(response , HttpStatus.OK);
 		
 	}
 
